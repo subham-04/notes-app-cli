0df1bdbf3c573748fe34586bb030d4878a877d33
Subham Bhadra <70264692+subham-04@users.noreply.github.com>
2023-02-28 20:38:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+import notes from './notes.js';
+import yargs from 'yargs';
+import {hideBin}from 'yargs/helpers'
+
+
+
+// const command = process.argv;  it gets the command line input
+
+
+const yargs_argv =yargs(hideBin(process.argv));
+
+// Add command
+
+yargs_argv.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.addNotes(argv.title, argv.body);
+        
+    }
+})
+
+// Remove command
+
+yargs_argv.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder:{
+        title:{ 
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNotes(argv.title);
+    }
+})
+
+// List command
+
+yargs_argv.command({
+    command: 'list',
+    describe: 'List note',
+    handler(){
+        notes.listNotes();
+    }
+})
+
+// Read command
+
+yargs_argv.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },    
+    handler(argv){
+        notes.readNote(argv.title);
+    }
+})
+
+
+yargs_argv.parse(); //this is the main thing * never remove *
+
